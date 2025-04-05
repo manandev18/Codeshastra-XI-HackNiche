d473cd74f09ce0964baed36c0eeeab24cd6662eb
Jagdish <jagdish.aade22@spit.ac.in>
2025-04-06 03:23:59 +0530

    Updated logo and background image, tweaked App layout

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,25 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        shimmer: "shimmer 5s ease-in-out infinite",
+      },
+      keyframes: {
+        shimmer: {
+          "0%, 100%": {
+            backgroundPosition: "0% 50%",
+          },
+          "50%": {
+            backgroundPosition: "100% 50%",
+          },
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
